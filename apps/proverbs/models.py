cbc59eb68740a70c69adb6614e0ef26f3d79090a
Bahodir Mansurov <6ahodir@gmail.com>
2013-04-23 17:06:31 -0400

    Add models to the Proverb app

--- apps/proverbs/models.py
+++ apps/proverbs/models.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.dispatch import receiver
+
+# How long should a game last?
+GAME_TIME_CHOICES = ((1, '1 min'), (3, '3 mins'), (5, '5 mins'))
+
+# Default game will last 3 minutes, make sure this number is in the
+# GAME_TIME_CHOICES tuple
+DEFAULT_GAME_TIME = 3
 
 
 class Proverb(models.Model):
@@ -8,6 +16,14 @@ class Proverb(models.Model):
 
     def __unicode__(self):
         return '%s' % self.text
+
+    def get_next_for_user(self, user):
+        """Get the next proverb for the user"""
+        proverb = Proverb.objects.filter(ProverbScore=None).order_by('?')
+        if proverb.count():
+            return proverb.latest()
+        else:
+            return ProverbScore.get_lowest_score_for_user(user)
 
 
 class ProverbScore(models.Model):
@@ -21,3 +37,50 @@ class ProverbScore(models.Model):
     def __unicode__(self):
         return '%s - %s - %d - %d' % (self.user.username, self.proverb[:20],
                                       self.view_count, self.correct_count)
+
+    @classmethod
+    def get_lowest_score_for_user(cls, user):
+        """Get the proverb that has the lowest correct_count / view_count
+        ratio
+        """
+        try:
+            return cls.objects.extra(
+                select={'score': 'correct_count / view_count'}
+            ).filter(user=user).latest('-score')
+        except cls.DoesNotExist:
+            return None
+
+
+class ScoreList(models.Model):
+    """User's top score
+    Scores are updated if a user's score is more than their top score.
+    """
+    user = models.OneToOneField(User)
+    score = models.PositiveIntegerField()
+
+    def __unicode__(self):
+        return '%s - %d' % (self.user.username, self.score)
+
+
+class UserProfile(models.Model):
+    """User's site settings
+    """
+    user = models.OneToOneField(User)
+    facebook_id = models.CharField(max_length=64)
+    # used to show the user's facebook photo on the score board
+    show_photo = models.BooleanField(default=True)
+    # used to show the user's facebook name on the score board
+    show_name = models.BooleanField(default=True)
+    # used to show the user's score on the score board
+    publish_score = models.BooleanField(default=True)
+    # used to decide the game length for the user
+    game_time = models.SmallPositiveIntegerField(choices=GAME_TIME_CHOICES,
+                                                 default=DEFAULT_GAME_TIME)
+
+
+@receiver(models.signals.post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """Create a user profile when a new user is created
+    """
+    if created:
+        profile, created = UserProfile.objects.get_or_create(user=instance)
