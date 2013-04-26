156b4185adb999cdf0a0ebee38c603994f5b9252
Bahodir Mansurov <6ahodir@gmail.com>
2013-04-26 01:30:49 -0400

    Make drag & drop working

--- apps/proverbs/static/proverbs/js/models/quiz.js
+++ apps/proverbs/static/proverbs/js/models/quiz.js
@@ -8,6 +8,24 @@ var app = app || {};
 	// ----------
 
 	app.Quiz = Backbone.Model.extend({
+        defaults: {
+            // answer will be populated once suggestions are dragged & dropped
+            answer: {}
+        },
 
+        initialize: function (options) {
+            this.setAnswer();
+        },
+
+        setAnswer: function () {
+            var answer = {};
+            _.each(this.get('question'), function (val, key) {
+                // ignore punctuation
+                if (val === 'u' || val === 'l') {
+                    answer[key] = "";
+                }
+            });
+            this.set('answer', answer);
+        }
 	});
 })();
--- apps/proverbs/static/proverbs/js/views/quiz.js
+++ apps/proverbs/static/proverbs/js/views/quiz.js
@@ -1,6 +1,6 @@
 var app = app || {};
 
-(function ($) {
+(function($) {
 	'use strict';
 
 	// Quiz View
@@ -15,28 +15,76 @@ var app = app || {};
 		events: {
 		},
 
-		initialize: function () {
-			//this.listenTo(this.model, 'change', this.render);
+		initialize: function() {
+			this.listenTo(this.model, 'change:question', this.model.setAnswer);
             this.render();
 		},
 
-		render: function () {
+		render: function() {
+            var that = this;
 			this.$el.html(this.template(this.model.toJSON()));
             var $question = this.$el.find('#question'),
-                $suggestions = this.$el.find('#suggestions');
+                $suggestions = this.$el.find('#suggestions'),
+                defaultPlaceholderWidth = $question.find('.placeholder:first').width();
             $('.suggestion', $suggestions).draggable({
                 opacity: 0.5,
-                //revert: 'invalid',
-                scope: 'quiz'//,
-                //snap: '.placeholder',
-                //snapMode: 'inner'
+                scope: 'quiz',
+                start: function (event, ui) {
+                    // clear the answer
+                    var suggestionKey = parseInt(ui.helper.data('key'), 10);
+                    _.each(that.model.get('answer'), function (val, key) {
+                        if (suggestionKey === val) {
+                            that.model.get('answer')[key] = '';
+                            $question.find('.placeholder[data-key=' + key + ']').droppable('option', 'disabled', false).css({width: defaultPlaceholderWidth});
+                            // move other dragged elements too because of the widht change above
+                            _.each(that.model.get('answer'), function (val2, key2) {
+                                if (val2) {
+                                    $suggestions.find('.suggestion[data-key=' + val2 + ']').offset(
+                                            $question.find('.placeholder[data-key=' + key2 + ']').offset()
+                                        );
+                                }
+                            });
+                        }
+                    });
+                },
+                stop: function (event, ui) {
+                    var suggestionKey = parseInt(ui.helper.data('key'), 10);
+                    // put to default position if not dropped onto a placeholder
+                    if (!_.contains(that.model.get('answer'), suggestionKey)) {
+                        ui.helper.animate({top: 0, left: 0}).removeClass('used').removeClass('capital');
+                    } else {
+                        _.each(that.model.get('answer'), function (val, key) {
+                            if (suggestionKey === val && that.model.get('question')[key] === 'u') {
+                                ui.helper.addClass('capital');
+                            }
+                        });
+                    }
+                }
             });
             $('#question .placeholder').droppable({
                 accept: '#suggestions .suggestion',
-                drop: function (event, ui) {
-                    //$(this).addClass( "ui-state-highlight").find( "p" )
-                        //.html( "Dropped!" );
+                drop: function(event, ui) {
+                    var $this = $(this),
+                        suggestionKey;
+                    if (ui.draggable) {
+                        that.model.get('answer')[$this.data('key')] = 
+                            ui.draggable.data('key');
+                        $this.droppable('option', 'disabled', true);
+                        ui.draggable.addClass('used');
+                        $this.css('width', ui.draggable.width() + 5); // for punctuation
+                        // move other dragged elements too because of the widht change above
+                        _.each(that.model.get('answer'), function (val, key) {
+                            if (val) {
+                                $suggestions.find('.suggestion[data-key=' + val + ']').offset(
+                                        $question.find('.placeholder[data-key=' + key + ']').offset()
+                                    );
+                            }
+                        });
+                    }
                 },
+                over: function(event, ui) {
+                },
+                tolerance: 'pointer',
                 scope: 'quiz'
             });
 			return this;
